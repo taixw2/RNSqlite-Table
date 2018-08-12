fb81c6e469718f4e98633cc42d74f15e42492dce
taixw2 <254225961@qq.com>
2018-08-12 23:57:52 +0800

    feat<extension>: 增加 isEmpty 和 isPlainObject 函数

--- utils/extension.ts
+++ utils/extension.ts
@@ -1,3 +1,6 @@
 
 export const flat = (array: any[]) => Array.prototype.concat.apply([], array);
 export const last = (array: any[]) => array[array.length - 1];
+
+export const objectIsEmpty = (object: object) => !Object.keys(object).length;
+export const isPlainObject = (object: object) => Object.prototype.toString.call(object) === "[object Object]";
