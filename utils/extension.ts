e1252613432f98d03a10e89a56277585bf10cd66
taixw2 <254225961@qq.com>
2018-08-13 23:15:13 +0800

    feat<util>: 新增对象根据字符串数组返回对应的排序数组对象

--- utils/extension.ts
+++ utils/extension.ts
@@ -4,3 +4,7 @@ export const last = (array: any[]) => array[array.length - 1];
 
 export const objectIsEmpty = (object: object) => !Object.keys(object).length;
 export const isPlainObject = (object: object) => Object.prototype.toString.call(object) === "[object Object]";
+
+export const soryObject = (object: { [key: string]: any }, keyRules: string[]) => {
+  return keyRules.map((key) => object[key]).filter((_) => !!_);
+};
