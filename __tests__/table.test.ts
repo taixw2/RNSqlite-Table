2b1777fa0c3f32327a7c1edef0d54c0174c5de63
taixw2 <254225961@qq.com>
2018-08-26 18:36:17 +0800

    fix<insert>: 解决 insert action 语法错误

--- __tests__/table.test.ts
+++ __tests__/table.test.ts
@@ -7,19 +7,19 @@ describe("insert", () => {
       .toEqual({ stmt: "INSERT INTO `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
   });
 
-  it("INSERT INTO OR REPLACE `User` (`name`, `age`) VALUES ('Ou', 10)", () => {
+  it("INSERT OR REPLACE INTO `User` (`name`, `age`) VALUES ('Ou', 10)", () => {
     expect(user.insert({ name: "Ou", age: 10 }, Const.WriteAction.REPLACE).query())
-      .toEqual({ stmt: "INSERT INTO OR REPLACE `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
+      .toEqual({ stmt: "INSERT OR REPLACE INTO `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
   });
 
-  it("INSERT INTO OR ABORT `User` (`name`, `age`) VALUES ('Ou', 10)", () => {
+  it("INSERT OR ABORT INTO `User` (`name`, `age`) VALUES ('Ou', 10)", () => {
     expect(user.insert({ name: "Ou", age: 10 }, Const.WriteAction.ABORT).query())
-      .toEqual({ stmt: "INSERT INTO OR ABORT `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
+      .toEqual({ stmt: "INSERT OR ABORT INTO `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
   });
 
-  it("INSERT INTO OR ROLLBACK `User` (`name`, `age`) VALUES ('Ou', 10)", () => {
+  it("INSERT OR ROLLBACK INTO `User` (`name`, `age`) VALUES ('Ou', 10)", () => {
     expect(user.insert({ name: "Ou", age: 10 }, Const.WriteAction.ROLLBACK).query())
-      .toEqual({ stmt: "INSERT INTO OR ROLLBACK `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
+      .toEqual({ stmt: "INSERT OR ROLLBACK INTO `User` (`name`,`age`) VALUES (?,?)", value: ["Ou", 10] });
   });
 });
 
